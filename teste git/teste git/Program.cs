85d3317ab93c02c70a8eafe3613fd0ae37bc2442
beatriz-minhos <138534948+BiaMinhos@users.noreply.github.com>
2024-03-01 16:22:56 -0300

    criação_do_arquivo_main.cs
    durante a aula criei o arquivo teste

---
+++ teste git/teste git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace teste_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = " Bibs Mianchi";
+            Console.WriteLine(nome);
+            Console.ReadKey();
+        }
+    }
+}
